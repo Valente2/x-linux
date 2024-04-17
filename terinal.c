39e6bffee9a364e94a143bff23afa9a63726cb40
I requst you to pull <136606905+Valente2@users.noreply.github.com>
2024-04-16 20:38:40 -0400

    Update terinal.c

--- terinal.c
+++ terinal.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-
-int main() {
-   int run = 1;
-   
-   while (run == 1){
-      char cmd[101];
-      fgets(",&cmd);
-      if (cmd=="exit")
-   } 
+#include <string.h>
+int define_commands(void){
+  char exit[5]="exit";
+}
+int main(){
+  define_commands();
+  int cmdout;
+  bool run = 1;
+  while (run==1){
+    char cmd[100];
+    printf("}");
+    fgets(cmd,100,);
+    cmdout=strcmp(exit,cmd);
+    if (cmdout == 0);{
+      run=0;
+    }
+    
+  }
 }
